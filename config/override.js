bcaa1ce4a68f45720644acf14a246455e4378575
Shridhar TL <shridhar.tl@gmail.com>
2023-11-24 14:00:27 +0530

    Initial version of custom react build script comitted

---
+++ config/override.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs-extra');
+const configPath = path.join(process.cwd(), 'build.config.js');
+
+let configOverride = {};
+
+if (fs.existsSync(configPath)) {
+    configOverride = require(configPath);
+}
+
+function applySettings() {
+    const defaultConfig = {
+        allowManifestFileGeneration: false,
+        allowSourceMapForCSS: false,
+    };
+
+    return { ...defaultConfig, ...configOverride.applySettings?.(defaultConfig) };
+}
+
+function overrideExtensions(fileExtensions) {
+    return configOverride.overrideExtensions?.(fileExtensions) ?? fileExtensions;
+}
+
+function overrideEntry(entry) {
+    return configOverride.overrideEntry?.(entry) ?? entry;
+}
+
+function overrideOutput(output) {
+    return configOverride.overrideOutput?.(output) ?? output;
+}
+
+function addPlugins(plugins) {
+    return configOverride.addPlugins?.(plugins) ?? plugins;
+}
+
+function overridePlugins(plugins) {
+    return configOverride.overridePlugins?.(plugins) ?? plugins;
+}
+
+function overrideWebPackConfig(config) {
+    return configOverride.overrideWebPackConfig?.(config) ?? config;
+}
+
+function onBuildComplete(args) {
+    return configOverride.onBuildComplete?.(args);
+}
+
+module.exports = {
+    overrideExtensions,
+    overrideEntry,
+    overrideOutput,
+    addPlugins,
+    applySettings,
+    overridePlugins,
+    overrideWebPackConfig,
+    onBuildComplete
+};
